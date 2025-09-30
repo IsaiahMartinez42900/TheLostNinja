fd7d158e271c54a52b32717ef70ddbb1b179dd23
IsaiahMartinez42900 <89949340+IsaiahMartinez42900@users.noreply.github.com>
2025-09-29 23:00:12 -0400

    Shuriken Cooldown Instantiate Fix

--- Assets/NinjaAction.cs
+++ Assets/NinjaAction.cs
@@ -1,30 +1,40 @@
+using System.Threading;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using static UnityEditorInternal.ReorderableList;
 
 public class NinjaAction : MonoBehaviour
-{
-
-    //public float speed = 15.5f;
+{  
     private Rigidbody2D rb;
     public float characterRunSpeed = 9.0f;
     public float characterWalkSpeed = 6.0f;
+
     public float shurikenSpeed = 15.0f;
     private bool hasLaunched = false;
     private bool isCharacter = false;
     private bool isShuriken = false;
+
     public float jumpForce = 10f;
     private bool isGrounded;
     public float direction = 1;
+    private float move;
+
+    private float shurikenTimer = 0f;
+    private float shurikenCooldown = 1.5f;
+    public int maxShurikens = 4;
+    private int currentShurikens;
+
+    //private int shurikenCounter = 0;
+   // private float cooldownTimer = 0f;
+    //private bool isOnCooldown;
+    
 
     private Animator anim;
-
-     public GameObject shurikenPrefab;
-    //public Transform shurikenSpawnPoint;
-    //public int shurikenCount = 3;
-
+    public GameObject shurikenPrefab;
+    
     void Start()
     {
+        currentShurikens = maxShurikens;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         if (this.CompareTag("Character"))
@@ -48,23 +58,27 @@ public class NinjaAction : MonoBehaviour
 
             //Character movement
 
-            float move = 0f;
+            //float move = 0f;
             int direction = 1;
             if (Input.GetKey(KeyCode.D))
             {
                 move = 1f;
                 direction = 1;
-                
+
             }
             else if (Input.GetKey(KeyCode.A))
             {
                 move = -1f;
                 direction = -1;
-                
+
+            }
+            else
+            {
+                move = 0;
             }
             if (move != 0f)
             {
-                
+
                 transform.Translate(Vector2.right * move * characterWalkSpeed * Time.deltaTime);
 
                 Vector3 scale = transform.localScale;
@@ -90,93 +104,62 @@ public class NinjaAction : MonoBehaviour
 
             }
         }
-        /*if (Input.GetKey(KeyCode.D))
+
+      // HELP NEEDED
+       /* if (isOnCooldown)
         {
-
-            transform.Translate(Vector2.right * characterSpeed * Time.deltaTime);
-            // transform.localScale = new Vector3(1, 1, 1);
-            int direction = 1;
-            Vector3 scale = transform.localScale;
-            scale.x = Mathf.Abs(scale.x) * direction;
-            transform.localScale = scale;
-            anim.SetBool("Walk", true);
-        }
-         else if (Input.GetKey(KeyCode.LeftShift))
-        {
-            anim.SetBool("Run", true);
-            anim.SetBool("Walk", false);
-        }
-        else
-        {
-            anim.SetBool("Walk", false);
-            anim.SetBool("Run", false);
-        }
-
-         if (Input.GetKey(KeyCode.A))
-        {
-
-            transform.Translate(Vector2.left * characterSpeed * Time.deltaTime);
-            //transform.localScale = new Vector3(-1, 1, 1);
-            int direction = -1;
-            Vector3 scale = transform.localScale;
-            scale.x = Mathf.Abs(scale.x) * direction;
-            transform.localScale = scale;
-            anim.SetBool("Walk", true);
-        }
-        else if (Input.GetKey(KeyCode.LeftShift))
-        {
-            anim.SetBool("Run", true);
-            anim.SetBool("Walk", false);
-        }
-        else
-        {
-             //anim.SetBool("Walk", true);
-            anim.SetBool("Run", false);
-            anim.SetBool("Walk", false);
-        }*/
-
-
-
-
-
-        if (isShuriken)
-        {
-            // Shuriken movement
-            if (Input.GetKeyDown(KeyCode.Space) && !hasLaunched) //&& shurikenCount > 0)
+            shurikenTimer -= Time.deltaTime;
+            if (shurikenTimer <= 0f)
             {
-
-                hasLaunched = true;
-               GameObject shuriken = Instantiate(shurikenPrefab, transform.position, transform.rotation);
-                Rigidbody2D rb = shuriken.GetComponent<Rigidbody2D>();
-                if (rb != null)
-                {
-                   // transform.Translate(Vector2.right * shurikenSpeed * Time.deltaTime);
-                    rb.AddForce(transform.right * shurikenSpeed, ForceMode2D.Impulse);
-                }
-               // shurikenCount--;
-               // Instantiate(bullet, spawnPoint.position, Quaternion.identity);
+                isOnCooldown = false;
+                currentShurikens = maxShurikens;
+            }
+        }*/
+       if (currentShurikens < maxShurikens)
+        {
+            if (shurikenTimer < shurikenCooldown)
+            {
+                shurikenTimer += Time.deltaTime;
 
             }
-           // if (hasLaunched)
-           // {
-
-                //Rigidbody2D rb = shurikenPrefab.GetComponent<Rigidbody2D>();
-               // if (rb != null)
-               // { 
-                //transform.Translate(Vector2.right * shurikenSpeed * Time.deltaTime);
-               // }
-
-            //}
+            else
+            {
+                shurikenTimer = 0;
+                currentShurikens++;
+            }
 
         }
+ 
+        if (Input.GetKeyDown(KeyCode.Space) && currentShurikens > 0) //&& !isOnCooldown)
+            {
+           // anim.SetTrigger("ShurikenThrow");
+            anim.SetBool("ShurikenThrow", true);
+
+            //hasLaunched = true;
+            GameObject s = Instantiate(shurikenPrefab, transform.position, transform.rotation);
+               s.GetComponent<Shuriken>().direction = direction;
+           // shurikenTimer = shurikenCooldown;
+    
+            currentShurikens--;
+             /*if (currentShurikens <= 3)
+            {
+                isOnCooldown = true;
+                shurikenTimer = 3f;
+            }   */     
+            } 
+        
+
+
+
+
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
-             
+
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
             anim.SetBool("Jump", true);
             Debug.Log("jumped");
-        
+
         }
     }
 
@@ -185,14 +168,7 @@ public class NinjaAction : MonoBehaviour
         void OnCollisionEnter2D(Collision2D collision)
         {
 
-            string myTag = this.tag;
 
-            if ((myTag == "CharacterShuriken" && collision.gameObject.CompareTag("EnemyNinja")))
-            {
-                Destroy(gameObject);
-                Destroy(collision.gameObject);
-
-            }
             if (collision.gameObject.CompareTag("Ground"))
             {
                 isGrounded = true;
---
+++ Assets/Shuriken.cs
@@ -0,0 +1,38 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Shuriken : MonoBehaviour
+{
+    private float speed = 20.5f;
+    public float direction;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = direction * speed * Time.deltaTime;
+        transform.Translate(xMove, 0, 0);
+        //transform.rotation = Quaternion.Euler(45, 0, 0);
+        //transform.eulerAngles = new Vector3(45, 0, 0);
+        transform.Rotate(30, 0 * Time.deltaTime, 0);
+
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        if ((collision.gameObject.CompareTag("EnemyNinja")))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+
+        }
+
+    }
+
+}
