23d3b6b54714d249628b6c3e034780e7da4ab8dc
IsaiahMartinez42900 <89949340+IsaiahMartinez42900@users.noreply.github.com>
2025-10-05 06:44:25 -0400

    Camera follow character

--- Assets/CharacterCamera.cs
+++ Assets/CharacterCamera.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class CharacterCamera : MonoBehaviour
 {
+    public Camera maincamera;
+    public float yOffset;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        maincamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.Position(Vector2.right * CameraSpeed * Time.deltaTime);
+        maincamera.transform.position = new Vector3(transform.position.x, maincamera.transform.position.y, maincamera.transform.position.z);
     }
 }
